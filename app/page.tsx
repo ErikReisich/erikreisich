aad436e2f510660fb3befcf66ce3e81a7d80fc3f
Erik <reisicherik@gmail.com>
2025-10-03 21:54:13 +0800

    Earth Image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Hello World!</h1>
-      <p className="mt-2 text-lg">I guess you have to start somewhere</p>
-    </main>
+    <>
+      <main className="flex flex-col justify-between items-center min-h-screen">
+        <h1 className="text-4xl font-bold mt-4">Hello World!</h1>
+        <p className="mt-2 text-lg mb-4">I guess you have to start somewhere</p>
+      </main>
+    </>
   );
 }
